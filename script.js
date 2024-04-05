1a8072dcac5c0b1b77bab696a67a8d4f01525d7d
Sindhia-raj96 <161922154+Sindhia-raj96@users.noreply.github.com>
2024-04-04 23:17:30 -0600

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let cardsWon = [];
 
     const cardArray = [
-        { name: 'card1', img: 'images/distracted.png' },
+        { name: 'card1', img: 'images/distracted.png'  },
         { name: 'card1', img: 'images/distracted.png' },
         { name: 'card2', img: 'images/drake.png' },
         { name: 'card2', img: 'images/drake.png' },
